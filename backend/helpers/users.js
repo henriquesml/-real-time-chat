c7c9ac9b9a6527a09a4068d53e7aa53f3f7c5ac5
henriquesml <henrique_schmeller@hotmail.com>
2020-04-12 14:04:55 -0300

    initial commit

---
+++ backend/helpers/users.js
@@ -0,0 +1,37 @@
+const users = []
+
+const addUser = ({ id, name, room }) => {
+
+  name = name.trim().toLowerCase()
+  room = room.trim().toLowerCase()
+
+  if(!name || !room) return { error: 'Apelido e sala são obrigátorios.' }
+  const userExists = users.find((user) => user.room === room && user.name === name)
+
+  if (userExists) {
+    return { error: 'Nome de usuário já esta sendo utilizado.'}
+  }
+
+  const user = { id, name, room }
+
+  users.push(user)
+
+  return { user }
+}
+
+const removeUser = (id) => {
+
+  const index = users.findIndex((user) => user.id === id)
+
+  if(index !== -1) {
+    return users.splice(index, 1)[0]
+  }
+
+}
+
+const getUser = (id) => users.find((user) => user.id === id)
+
+const getUsersInRoom = (room) => users.filter((user) => user.room === room)
+
+
+module.exports = { addUser, removeUser, getUser, getUsersInRoom }
