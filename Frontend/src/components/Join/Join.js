2e4252942e7a6e6b65171ab67351860d67b1323c
Henrique Schmeller <henrique_schmeller@hotmail.com>
2020-04-13 11:32:35 -0300

    ajustes

--- Frontend/src/components/Join/Join.js
+++ Frontend/src/components/Join/Join.js
@@ -10,12 +10,12 @@ export default function SignIn() {
   return (
     <div className="joinOuterContainer">
       <div className="joinInnerContainer">
-        <h1 className="heading">Entrar</h1>
+        <h1 className="heading">Chat</h1>
         <div>
-          <input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
+          <input placeholder="Seu nome" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
         </div>
         <div>
-          <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />
+          <input placeholder="Sala" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />
         </div>
         <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
           <button className={'button mt-20'} type="submit">Entrar</button>
